1b36b8c3736a1446e33742f0be718bd6b5977e11
Mridul <mridularafin@gmail.com>
2022-09-13 23:18:52 +0600

    layout created

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+//EXAMINE THE DOCUMENT OBJECT
 console.dir(document);
+
+//log() method displays the “toString” representation of any object passed to it
+console.log(document.all);
+
+//dir() method displays an interactive list of the properties of the specified JavaScript object
 console.dir(document.all);
+
+console.dir(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+
+//Changing stuffs
+document.title = "Movies Watched";
