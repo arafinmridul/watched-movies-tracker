8e66364d3981b13f136542b7178fb8776b76a72d
Mridul <mridularafin@gmail.com>
2022-09-16 16:03:25 +0600

    button added

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let form = document.getElementById("addForm");
 let itemList = document.getElementById("items");
 
 //Submit
-form.addEventListener("submit", addItem);
+form.addEventListener("submit", addMovie);
 
-//Adding Items
-function addItem(e) {
+//Adding Movies
+function addMovie(e) {
   e.preventDefault();
 
   //Grabbing Inputs
@@ -15,10 +15,25 @@ function addItem(e) {
   let li = document.createElement("li");
 
   //Setting things to the list
+  //class
   li.className = "list-group-item listStyle";
   //   console.log(li);
+  //text
   li.appendChild(document.createTextNode(newItem));
 
   //Appending the new list into the ul
   itemList.appendChild(li);
+
+  //Create delete button
+  let button = document.createElement("button");
+
+  //Setting things to the button
+  //class
+  button.className = "btn btn-danger btn-sm float-right delete";
+  // console.log(button);
+  //text
+  button.appendChild(document.createTextNode("x"));
+
+  //Appending the button into the li
+  li.appendChild(button);
 }
